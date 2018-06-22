763b00eec442ff2dcff678dddd87b6d1aae9a3ad
Andrew Vandermeer <andrew.vandermeer@mbp-1shtdf.bs.local>
2018-06-22 10:51:48 -0600

    route activator for a protected route to a 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppComponent } from './app.component';
 import { EventsListComponent } from './events-list/events-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { EventDetailsComponent } from './event-details/event-details.component';
+import { CreateEventComponent } from './create-event/create-event.component';
+import { Error404Component } from './errors/404.component';
 
 import { ToastrService } from './common/toastr.service';
 import { EventListService } from './events-list/shared/events-list.service';
+import { EventRouteAcitivator } from './event-details/event-route-activator.service';
 import { appRoutes } from './common/routes';
 
 @NgModule({
@@ -17,12 +20,18 @@ import { appRoutes } from './common/routes';
     EventsListComponent,
     NavbarComponent,
     EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
-  providers: [EventListService, ToastrService],
+  providers: [
+    EventListService,
+    ToastrService,
+    EventRouteAcitivator
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
