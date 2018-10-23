d935c8faf8534bbc1ef764034fbb3294973b9406
Andrew Vandermeer <innerchaosfitness@yahoo.com>
2018-10-23 17:08:23 -0600

    Added pipes, spacing, and ng-content (#9)
    * Linting files
    
    * Added pipes, spacing, and ng-content

--- src/app/common/collapsible-well/collapsible-well.component.ts
+++ src/app/common/collapsible-well/collapsible-well.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./collapsible-well.component.css']
 })
 export class CollapsibleWellComponent implements OnInit {
-  @Input() title: string;
   visible: Boolean = true;
 
   constructor() { }
